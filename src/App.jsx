2028ee5a8cc42612108bade4cc46a5aff0b55c66
aum singhal <aumi@aums-Laptop.local>
2024-05-18 16:44:48 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [eurusd , setEurusd] = useState(1.04);
+  const [eurusd, setEurusd] = useState(1.04);
   const [usdinr, setUsdinr] = useState(80.05);
   const [audusd, setAudusd] = useState(0.67);
   const currency = ["USD", "EUR", "INR", "AUD"];
@@ -11,15 +11,15 @@ function App() {
   const [source, setSource] = useState("INR");
   const [target, setTarget] = useState("USD");
   const marketRates = [
-    {currency: "EUR/USD", amount: eurusd},
-    {currency: ".USD/INR", amount: usdinr},
-    {currency: "AUD/USD", amount: audusd}
+    { currency: "EUR/USD", amount: eurusd },
+    { currency: ".USD/INR", amount: usdinr },
+    { currency: "AUD/USD", amount: audusd }
   ]
 
   useEffect(() => {
     let id;
     const api = () => {
-      let temp = ((Math.random() * 6)-3)/ 100;
+      let temp = ((Math.random() * 6) - 3) / 100;
       setEurusd(eurusd + (eurusd * temp));
       setUsdinr(usdinr + (usdinr * temp));
       setAudusd(audusd + (audusd * temp));
@@ -27,22 +27,22 @@ function App() {
     };
     api();
     return () => {
-    clearTimeout(id);
+      clearTimeout(id);
     };
   }, [])
 
   const getValue = () => {
-    if(source === "USD" && target === "EUR" || source === "EUR" && target === "USD"){return (amount * eurusd).toFixed(2)}
-    else if(source === "USD" && target === "INR" || source === "INR" && target === "USD"){return (amount * usdinr).toFixed(2)}
-    else if(source === "USD" && target === "AUD" || source === "AUD" && target === "USD"){return (amount * audusd).toFixed(2)}
-    else { return "ERROR!!"}
+    if (source === "USD" && target === "EUR" || source === "EUR" && target === "USD") { return (amount * eurusd).toFixed(2) }
+    else if (source === "USD" && target === "INR" || source === "INR" && target === "USD") { return (amount * usdinr).toFixed(2) }
+    else if (source === "USD" && target === "AUD" || source === "AUD" && target === "USD") { return (amount * audusd).toFixed(2) }
+    else { return "ERROR!!" }
   }
 
   const CurrencyDropDown = (props) => {
     return <div className="currency-dropdown">
       <div className="font-15">{props.title}</div>
 
-      <select defaultValue={props.value} onChange={e => props.set(e.target.vlaue)} name={props.id} id={props.id}>
+      <select value={props.value} onChange={e => props.set(e.target.value)} name={props.id} id={props.id}>
         {currency.map(item => {
           return <option value={item} key={item}>{item}</option>
         })}
@@ -56,7 +56,7 @@ function App() {
         <div className="font-30 bold">Market</div>
 
         <table className="market-chart">
-          {marketRates.map((item, index)=>{
+          {marketRates.map((item, index) => {
             return (
               <tr className="market-chart-item" key={index}>
                 <td className="market-chart-item-currency">{item.currency}</td>
@@ -66,15 +66,35 @@ function App() {
           })}
         </table>
       </div>
-      
+
       <div className="currency flex-d-col">
         <div className="font-30 bold text-center">
           Currency
         </div>
 
-        <CurrencyDropDown value={source} currency={currency} id={"source"} set={setSource} title={"Source currency"} />
+        {/* <CurrencyDropDown value={source} currency={currency} id={"source"} set={setSource} title={"Source currency"} />
 
-        <CurrencyDropDown value={target} currency={currency} id={"target"} set={setTarget} title={"Target currency"} />
+        <CurrencyDropDown value={target} currency={currency} id={"target"} set={setTarget} title={"Target currency"} /> */}
+
+        <div className="currency-dropdown">
+          <div className="font-15">Source Currency</div>
+
+          <select value={source} onChange={e => setSource(e.target.value)} name="source-drop" >
+            {currency.map(item => {
+              return <option value={item} key={item}>{item}</option>
+            })}
+          </select>
+        </div>
+
+        <div className="currency-dropdown">
+          <div className="font-15">Target currency</div>
+
+          <select value={target} onChange={e => setTarget(e.target.value)} name="target-drop" >
+            {currency.map(item => {
+              return <option value={item} key={item}>{item}</option>
+            })}
+          </select>
+        </div>
 
         <div className="amount">
           <div className="font-15">Amount</div>
