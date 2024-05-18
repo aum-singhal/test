18d8d3fe5140d8883d2c71ba65caec25aeaf69ea
aum singhal <aumi@aums-Laptop.local>
2024-05-18 17:52:05 +0530

    adding error card

--- src/components/currency/currency.jsx
+++ src/components/currency/currency.jsx
@@ -3,32 +3,36 @@ import "./currency.css";
 
 export const CurrencyComponent = ( props ) => {
   const currency = ["USD", "EUR", "INR", "AUD"];
+  const [showError, setShowError] = useState(false);
   const [amount, setAmount] = useState(0)
-  const [source, setSource] = useState("INR");
-  const [target, setTarget] = useState("USD");
+  const [source, setSource] = useState("USD");
+  const [target, setTarget] = useState("INR");
   const [value, setValue] = useState(0);
-
-  const CurrencyDropDown = (props) => {
-    return <div className="currency-dropdown">
-      <div className="font-15">{props.title}</div>
-
-      <select value={props.value} onChange={e => props.set(e.target.vlaue)} name={props.id} id={props.id}>
-        {currency.map(item => {
-          return <option value={item} key={item}>{item}</option>
-        })}
-      </select>
-    </div>
-  }
+  const [text, setText] = useState("")
 
   const getValue = () => {
-    if (source === "USD" && target === "EUR" || source === "EUR" && target === "USD") { setValue((amount * props.eurusd).toFixed(2)) }
-    else if (source === "USD" && target === "INR" || source === "INR" && target === "USD") { setValue((amount * props.usdinr).toFixed(2)) }
-    else if (source === "USD" && target === "AUD" || source === "AUD" && target === "USD") { setValue((amount * props.audusd).toFixed(2)) }
-    else { return "ERROR!!" }
+    if (source === "EUR" && target === "USD") { setValue((amount / props.eurusd).toFixed(2)) }
+    else if (source === "INR" && target === "USD") { setValue((amount / props.usdinr).toFixed(2)) }
+    else if (source === "AUD" && target === "USD") { setValue((amount / props.audusd).toFixed(2)) }
+    if (source === "USD" && target === "EUR") { setValue((amount * props.eurusd).toFixed(2)) }
+    else if (source === "USD" && target === "INR") { setValue((amount * props.usdinr).toFixed(2)) }
+    else if (source === "USD" && target === "AUD") { setValue((amount * props.audusd).toFixed(2)) }
+    else if(source === target){setText("source and target currency cannot be the same"); setShowError(true)}
+    else { setText("currency cannot be converted, please look at market again carefully!"); setShowError(true) }
+  }
+
+  const ErrorCard = () => {
+    return <div className="error-card flex-a-cen-j-cen">
+      <div className="error-container">
+        <div onClick={()=>setShowError(false)} className="close">X</div>
+        {text}
+      </div>
+    </div>
   }
 
   return (
     <div className="currency flex-d-col">
+      {showError? <ErrorCard /> : ""}
       <div className="font-30 bold text-center">
         Currency
       </div>
@@ -67,24 +71,4 @@ export const CurrencyComponent = ( props ) => {
       <button onClick={() => getValue()} className="button bold font-21">Exchange</button>
     </div>
   );
-}
-
-const CustomDropdown = () => {
-  return <div className="dropdown">
-    <div onClick={(e) => { setIsActive(!isActive); }} className="dropdown-btn">
-      {props.value}
-      <span className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"} />
-    </div>
-    <div className="dropdown-content" style={{ display: isActive ? "block" : "none" }}>
-      {
-        currency.map((item, index) => {
-          return (
-            <div key={index} onClick={(e) => {
-              props.set(e.target.textContent); setIsActive(!isActive);
-            }} className="dropdown-item">{item}</div>
-          )
-        })
-      }
-    </div>
-  </div>
 }
